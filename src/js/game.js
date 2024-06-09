6148131ffccd1b4c8e33da167284c219f5657be8
hanbit10 <chanbit10@gmail.com>
2024-06-09 13:48:44 +0200

    classes

--- src/js/game.js
+++ src/js/game.js
@@ -1,10 +1,14 @@
 let canvas;
-let character = new Image();
+let character = new Character()
+let enemies = [
+  new Chicken(),
+  new Chicken(),
+  new Chicken()
+]
 let ctx;
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d")
 
-  character.src = '/assets/img/2_character_pepe/2_walk/W-21.png'
-  ctx.drawImage(character, 0, 0, 50, 100, 0, 0, 700, 600);
+  console.log("my Character", character)
 }
---
+++ src/models/movable-object.class.js
@@ -0,0 +1,17 @@
+class MovableObject {
+  x = 120
+  y = 400
+  img 
+
+  moveRight() {
+    console.log("moveRight")
+  }
+
+  moveLeft(){
+    console.log("moveLeft")
+  }
+
+  jump(){
+    console.log("jump")
+  }
+}
