ac6f360b7217e3eb380729d122e9f180dafffd7e
hanbit10 <chanbit10@gmail.com>
2024-06-09 19:51:14 +0200

    move character

--- src/models/character.class.js
+++ src/models/character.class.js
@@ -9,6 +9,7 @@ class Character extends MovableObject {
     "/assets/img/2_character_pepe/2_walk/W-26.png",
   ]
   world;
+  speed = 5
   constructor(){
     super().loadImage("/assets/img/2_character_pepe/2_walk/W-21.png")
     this.loadImages(this.IMAGES_WALKING)
@@ -18,14 +19,21 @@ class Character extends MovableObject {
   animate() {
     setInterval(() => {
       if(this.world.keyboard.RIGHT) {
+        this.x = this.x+this.speed
+      }
+
+      if(this.world.keyboard.LEFT) {
+        this.x = this.x-this.speed
+      }
+    }, 1000/60)
+
+    setInterval(() => {
+      if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
         let i = this.currentImage % this.IMAGES_WALKING.length
         let path = this.IMAGES_WALKING[i]
         this.img = this.imageCache[path]
         this.currentImage++
-        this.x = this.x+5
       }
-      
-
-    }, 130)
+    }, 100)
   }
 }
