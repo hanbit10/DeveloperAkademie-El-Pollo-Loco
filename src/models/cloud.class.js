69df298fc409cb4979b3a13dcfc869ef37cceef2
hanbit10 <chanbit10@gmail.com>
2024-06-09 17:38:11 +0200

    move clouds

--- src/models/cloud.class.js
+++ src/models/cloud.class.js
@@ -5,5 +5,12 @@ class Cloud extends MovableObject {
   constructor(){
     super().loadImage("/assets/img/5_background/layers/4_clouds/1.png")
     this.x = 200+Math.random()*500;
+    this.animate()
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15
+    }, 1000/60)
   }
 }
