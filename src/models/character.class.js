e232738521b0122fe289e69121e741bc62b6cf97
hanbit10 <chanbit10@gmail.com>
2024-06-09 18:17:41 +0200

    animate

--- src/models/character.class.js
+++ src/models/character.class.js
@@ -1,14 +1,28 @@
 class Character extends MovableObject {
   height = 280;
   y = 170;
+  IMAGES_WALKING = ["/assets/img/2_character_pepe/2_walk/W-21.png",
+    "/assets/img/2_character_pepe/2_walk/W-22.png",
+    "/assets/img/2_character_pepe/2_walk/W-23.png",
+    "/assets/img/2_character_pepe/2_walk/W-24.png",
+    "/assets/img/2_character_pepe/2_walk/W-25.png",
+    "/assets/img/2_character_pepe/2_walk/W-26.png",
+  ]
+
+  currentImage = 0;
   constructor(){
     super().loadImage("/assets/img/2_character_pepe/2_walk/W-21.png")
-    this.loadImages(["/assets/img/2_character_pepe/2_walk/W-21.png",
-      "/assets/img/2_character_pepe/2_walk/W-22.png",
-      "/assets/img/2_character_pepe/2_walk/W-23.png",
-      "/assets/img/2_character_pepe/2_walk/W-24.png",
-      "/assets/img/2_character_pepe/2_walk/W-25.png",
-      "/assets/img/2_character_pepe/2_walk/W-26.png",
-    ])
+    this.loadImages(this.IMAGES_WALKING)
+    this.animate()
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length
+      let path = this.IMAGES_WALKING[i]
+      this.img = this.imageCache[path]
+      this.currentImage++
+    }, 130)
+
   }
 }
