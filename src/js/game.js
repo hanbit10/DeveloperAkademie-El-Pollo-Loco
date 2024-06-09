0c0b45434e94de36647437650d4819a32eb5bc88
hanbit10 <chanbit10@gmail.com>
2024-06-09 13:53:00 +0200

    everthing in world class

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,10 @@
 let canvas;
-let character = new Character()
-let enemies = [
-  new Chicken(),
-  new Chicken(),
-  new Chicken()
-]
+let world = new World()
+
 let ctx;
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d")
 
-  console.log("my Character", character)
+  console.log("my Character", world.character)
 }
--- src/models/world.class.js
+++ src/models/world.class.js
@@ -1,4 +1,10 @@
 class World {
+  character = new Character()
+  enemies = [
+    new Chicken(),
+    new Chicken(),
+    new Chicken()
+  ]
   draw() {
     console.log("draw")
   }
