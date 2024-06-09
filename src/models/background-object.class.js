b9b0249aa24f3625a2c8bae2b6e510aa63f02fc4
hanbit10 <chanbit10@gmail.com>
2024-06-09 15:39:00 +0200

    backgroud img scaling

--- src/models/background-object.class.js
+++ src/models/background-object.class.js
@@ -1,5 +1,10 @@
 class BackgroundObject extends MovableObject {
-  constructor(imagePath){
+  width = 720
+  height = 400
+
+  constructor(imagePath,x,y){
     super().loadImage(imagePath);
+    this.y = y;
+    this.x = x
   }
 }
--- src/models/world.class.js
+++ src/models/world.class.js
@@ -9,7 +9,7 @@ class World {
     new Cloud(),
   ]
   backgroundObjects = [
-    new BackgroundObject("/assets/img/5_background/layers/1_first_layer/1.png"),
+    new BackgroundObject("/assets/img/5_background/layers/3_third_layer/1.png", 0, 80),
   ]
 
   constructor(canvas) {
