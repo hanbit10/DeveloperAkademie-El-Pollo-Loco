80dde2e3497444ac6db65e51c669bc336098d3d4
hanbit10 <chanbit10@gmail.com>
2024-06-09 19:12:08 +0200

    keydown eventlistener

--- src/js/game.js
+++ src/js/game.js
@@ -3,4 +3,10 @@ let world
 function init() {
   canvas = document.getElementById("canvas");
   world = new World(canvas);
+  
+
 }
+
+document.addEventListener('keydown', (event) => {
+  console.log(event)
+}) 
