13a110d6ec2361017efa40179bbc7982987cf1c7
hanbit10 <chanbit10@gmail.com>
2024-06-09 17:23:13 +0200

    backgroundobjects

--- src/models/background-object.class.js
+++ src/models/background-object.class.js
@@ -1,10 +1,10 @@
 class BackgroundObject extends MovableObject {
   width = 720
-  height = 400
+  height = 480
+  y = 0
 
-  constructor(imagePath,x,y){
+  constructor(imagePath,x){
     super().loadImage(imagePath);
-    this.y = y;
     this.x = x
   }
 }
