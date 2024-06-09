b70b40243954b3461341577ccaa2f02a2985ce0f
hanbit10 <chanbit10@gmail.com>
2024-06-09 17:27:33 +0200

    set characters

--- src/models/character.class.js
+++ src/models/character.class.js
@@ -1,4 +1,6 @@
 class Character extends MovableObject {
+  height = 280;
+  y = 170;
   constructor(){
     super().loadImage("/assets/img/2_character_pepe/2_walk/W-21.png")
   }
--- src/models/chicken.class.js
+++ src/models/chicken.class.js
@@ -1,7 +1,9 @@
 class Chicken extends MovableObject {
+  height = 60
+  width = 60
+  y = 380
   constructor() {
     super().loadImage("/assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png")
     this.x = 200+Math.random()*500
-    this.y = 300
   }
 }
