0b2447727638156f89657b2a1c6677b289319c77
hanbit10 <chanbit10@gmail.com>
2024-06-10 19:12:03 +0200

    collision detection

--- src/models/movable-object.class.js
+++ src/models/movable-object.class.js
@@ -62,6 +62,13 @@ class MovableObject {
     }
   }
 
+  isColliding(mo) {
+    return  this.x + this.width >= mo.x && 
+            this.y + this.height >= mo.y &&
+            this.x <= mo.x&& 
+            this.y < mo.y + mo.height
+}
+
   playAnimation(images) {
     let i = this.currentImage % images.length
     let path = images[i]
