e47198d9c651ec7fe1bc4caf92d6395e587ae0a8
hanbit10 <chanbit10@gmail.com>
2024-06-12 01:53:53 +0200

    Bottle throw at enemies

--- src/models/chicken.class.js
+++ src/models/chicken.class.js
@@ -13,23 +13,35 @@ class Chicken extends MoveableObject {
   ]
 
   buck_sound = new Audio("/assets/audio/chicken/small-chicken/buck.wav")
-
-  constructor(x) {
+  moving
+  animateImgs
+  constructor(id, x) {
     super().loadImage("/assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png")
     this.loadImages(this.IMAGES_WALKING)
     this.x = x+Math.random()*100
+    this.id = id
     this.animate()
   }
   animate(){
-    setInterval(() => {
+    this.animateImgs = setInterval(() => {
       let i = this.currentImage % this.IMAGES_WALKING.length
       let path = this.IMAGES_WALKING[i]
       this.img = this.imageCache[path]
       this.currentImage++
     }, 130)
 
-    setInterval(() => { 
+    this.moving =setInterval(() => { 
       this.moveLeft(this.speed)
     }, 1000/60)
+  }
+
+  dead() {
+    this.loadImage("/assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png")
+    clearInterval(this.moving)
+    clearInterval(this.animateImgs)
+    setTimeout(() => {
+
+      this.y = 600
+    }, 2000)
   }
 }
