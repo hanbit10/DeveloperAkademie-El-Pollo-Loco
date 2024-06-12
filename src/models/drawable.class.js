41f1927fe9b97c4d2ac0e28df5d2fc41d491871c
hanbit10 <chanbit10@gmail.com>
2024-06-12 09:07:31 +0200

    format done

--- src/models/drawable.class.js
+++ src/models/drawable.class.js
@@ -1,48 +1,54 @@
 class DrawableObject {
-  img
-  imageCache = [] 
-  x = 100
-  y = 280
-  height = 150
-  width = 100
-  currentImage = 0
+  img;
+  imageCache = [];
+  x = 100;
+  y = 280;
+  height = 150;
+  width = 100;
+  currentImage = 0;
 
   loadImage(path) {
-    this.img = new Image()
-    this.img.src = path
+    this.img = new Image();
+    this.img.src = path;
   }
 
   loadImages(arr) {
     arr.forEach((path) => {
-      let img = new Image()
-      img.src = path
-      this.imageCache[path] = img
-    })
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
   }
 
   draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
   drawFrame(ctx, mo) {
-    if(this instanceof Chicken || this instanceof Endboss || this instanceof ChickenNormal || this instanceof Coin || this instanceof Bottle) {
+    if (
+      this instanceof Chicken ||
+      this instanceof Endboss ||
+      this instanceof ChickenNormal ||
+      this instanceof Coin ||
+      this instanceof Bottle
+    ) {
       ctx.beginPath();
-      ctx.lineWidth = "5"
-      ctx.strokeStyle = "blue"
-      ctx.rect(this.x, this.y, mo.frameWidth, mo.frameHeight)
-      ctx.stroke()
+      ctx.lineWidth = "5";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, mo.frameWidth, mo.frameHeight);
+      ctx.stroke();
     }
-    if(this instanceof Character) {
-      this.setCharacterFrame(ctx, mo)
+    if (this instanceof Character) {
+      this.setCharacterFrame(ctx, mo);
     }
   }
 
-  setCharacterFrame(ctx, mo){
+  setCharacterFrame(ctx, mo) {
     ctx.beginPath();
-    ctx.lineWidth = "2"
-    ctx.strokeStyle = "blue"
+    ctx.lineWidth = "2";
+    ctx.strokeStyle = "blue";
     // ctx.strokeOpacity = 0
-    ctx.rect(this.x+25, this.y+130, mo.frameWidth, mo.frameHeight)
-    ctx.stroke()
+    ctx.rect(this.x + 25, this.y + 130, mo.frameWidth, mo.frameHeight);
+    ctx.stroke();
   }
 }
--- src/models/world.class.js
+++ src/models/world.class.js
@@ -1,136 +1,134 @@
 class World {
-  character = new Character()
-  statusBar = new StatusBar()
-  throwableObjects = [new ThrowableObject()]
-  level = level1
-  enemies = this.level.enemies
-  clouds = this.level.clouds
-  coins = this.level.coins
-  bottles = this.level.bottles
-  backgroundObjects = this.level.backgroundObjects
-  camera_x = 0
-  alreadyCollided = [false]
-  enemyDead = [false, false, false, false, false]
-
+  character = new Character();
+  statusBar = new StatusBar();
+  throwableObjects = [new ThrowableObject()];
+  level = level1;
+  enemies = this.level.enemies;
+  clouds = this.level.clouds;
+  coins = this.level.coins;
+  bottles = this.level.bottles;
+  backgroundObjects = this.level.backgroundObjects;
+  camera_x = 0;
+  alreadyCollided = [false];
+  enemyDead = [false, false, false, false, false];
 
   constructor(canvas, keyboard) {
-    this.ctx = canvas.getContext('2d')
-    this.canvas = canvas
-    this.keyboard = keyboard
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.keyboard = keyboard;
     this.draw();
-    this.setWorld()
-    this.run()
-  }
-  
-  setWorld(){
-    this.character.world = this
+    this.setWorld();
+    this.run();
   }
 
-  run(){
+  setWorld() {
+    this.character.world = this;
+  }
+
+  run() {
     setInterval(() => {
-      this.checkThrowObjects()
-
-    }, 100)
+      this.checkThrowObjects();
+    }, 100);
 
     setInterval(() => {
-      this.checkCollisions()
-    }, 100)
+      this.checkCollisions();
+    }, 100);
   }
 
-  checkThrowObjects(){
-    if(this.keyboard.D) {
-      let bottle = new ThrowableObject(this.character.x, this.character.y)
-      this.throwableObjects.push(bottle)
-      this.alreadyCollided.push(false)
-      bottle.throwableCondition("throwing")
+  checkThrowObjects() {
+    if (this.keyboard.D) {
+      let bottle = new ThrowableObject(this.character.x, this.character.y);
+      this.throwableObjects.push(bottle);
+      this.alreadyCollided.push(false);
+      bottle.throwableCondition("throwing");
     }
-  } 
-  checkCollisions(){
-    let enemies = 0
+  }
+  checkCollisions() {
+    let enemies = 0;
     this.level.enemies.forEach((enemy) => {
-      if(this.character.isColliding(enemy)) {
-        console.log(this.enemyDead[enemies] == false)
-        if(this.enemyDead[enemies] == false) {
-          this.character.hit()
-          this.statusBar.setPercentage(this.character.energy)
+      if (this.character.isColliding(enemy)) {
+        console.log(this.enemyDead[enemies] == false);
+        if (this.enemyDead[enemies] == false) {
+          this.character.hit();
+          this.statusBar.setPercentage(this.character.energy);
         }
       }
-      let i = 0
+      let i = 0;
       this.throwableObjects.forEach((throwableObject) => {
-        if(throwableObject.isColliding(enemy)) {
-          if(this.alreadyCollided[i] == false && this.enemyDead[enemies] == false) {
+        if (throwableObject.isColliding(enemy)) {
+          if (this.alreadyCollided[i] == false && this.enemyDead[enemies] == false) {
             // console.log("collided")
-            throwableObject.throwableCondition("breaking")
+            throwableObject.throwableCondition("breaking");
           }
-          this.alreadyCollided[i] = true
-          console.log(enemy.id)
-          enemy.dead()
-          this.enemyDead[enemy.id] = true
+          this.alreadyCollided[i] = true;
+          console.log(enemy.id);
+          enemy.dead();
+          this.enemyDead[enemy.id] = true;
         }
-        i++
-      })
-      enemies++
-    })
+        i++;
+      });
+      enemies++;
+    });
 
     this.level.coins.forEach((coin) => {
-      if(this.character.isColliding(coin)) {
-        coin.collect("coin")
+      if (this.character.isColliding(coin)) {
+        coin.collect("coin");
       }
-
-    })
+    });
     this.level.bottles.forEach((bottle) => {
-      if(this.character.isColliding(bottle)) {
-        bottle.collect("bottle")
+      if (this.character.isColliding(bottle)) {
+        bottle.collect("bottle");
       }
-    })
+    });
   }
   draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    this.ctx.translate(this.camera_x, 0)
-    this.addObjectsToMap(this.backgroundObjects)
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.translate(this.camera_x, 0);
+    this.addObjectsToMap(this.backgroundObjects);
 
-    this.ctx.translate(-this.camera_x, 0)
-    this.addToMap(this.statusBar)
-    this.ctx.translate(this.camera_x, 0)
+    this.ctx.translate(-this.camera_x, 0);
+    this.addToMap(this.statusBar);
+    this.ctx.translate(this.camera_x, 0);
 
-    this.addObjectsToMap(this.clouds)
-    this.addObjectsToMap(this.throwableObjects)
-    this.addObjectsToMap(this.coins)
-    this.addObjectsToMap(this.bottles)
-    this.addObjectsToMap(this.enemies)
-    this.addToMap(this.character)
-    this.ctx.translate(-this.camera_x, 0)
+    this.addObjectsToMap(this.clouds);
+    this.addObjectsToMap(this.throwableObjects);
+    this.addObjectsToMap(this.coins);
+    this.addObjectsToMap(this.bottles);
+    this.addObjectsToMap(this.enemies);
+    this.addToMap(this.character);
+    this.ctx.translate(-this.camera_x, 0);
     //draw wird immer aufgerufen
     let self = this;
-    requestAnimationFrame(function() {
-      self.draw()})
+    requestAnimationFrame(function () {
+      self.draw();
+    });
   }
 
-  addObjectsToMap(objs){
-    objs.forEach(obj => {
-      this.addToMap(obj)
-    })
+  addObjectsToMap(objs) {
+    objs.forEach((obj) => {
+      this.addToMap(obj);
+    });
   }
-  addToMap(mo){
-    if(mo.otherDirection){
-      this.flipImage(mo)
+  addToMap(mo) {
+    if (mo.otherDirection) {
+      this.flipImage(mo);
     }
-    mo.draw(this.ctx)
-    mo.drawFrame(this.ctx, mo)
-    if(mo.otherDirection){
-      this.flipImageBack(mo)
+    mo.draw(this.ctx);
+    mo.drawFrame(this.ctx, mo);
+    if (mo.otherDirection) {
+      this.flipImageBack(mo);
     }
   }
 
   flipImage(mo) {
-    this.ctx.save()
-    this.ctx.translate(mo.width, 0)
-    this.ctx.scale(-1, 1)
-    mo.x = mo.x*-1
+    this.ctx.save();
+    this.ctx.translate(mo.width, 0);
+    this.ctx.scale(-1, 1);
+    mo.x = mo.x * -1;
   }
 
   flipImageBack(mo) {
-    mo.x = mo.x *-1
-    this.ctx.restore()
+    mo.x = mo.x * -1;
+    this.ctx.restore();
   }
 }
