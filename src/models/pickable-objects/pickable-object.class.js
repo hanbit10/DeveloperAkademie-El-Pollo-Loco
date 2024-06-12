16679cf87490141201647e6de24979bf89703397
hanbit10 <chanbit10@gmail.com>
2024-06-12 09:07:31 +0200

    format done

--- src/models/pickable-objects/pickable-object.class.js
+++ src/models/pickable-objects/pickable-object.class.js
@@ -1,18 +1,17 @@
 class PickableObject extends DrawableObject {
-
-  coin_collect = new Audio("/assets/audio/pickable/coins.wav")
-  bottle_collect = new Audio("/assets/audio/pickable/bottles.wav")
+  coin_collect = new Audio("/assets/audio/pickable/coins.wav");
+  bottle_collect = new Audio("/assets/audio/pickable/bottles.wav");
   constructor() {
     super();
   }
 
-  collect(pickable){
-    this.y = 600
-    if(pickable == "coin") {
-      this.coin_collect.play()
+  collect(pickable) {
+    this.y = 600;
+    if (pickable == "coin") {
+      this.coin_collect.play();
     }
-    if(pickable == "bottle") {
-      this.bottle_collect.play()
+    if (pickable == "bottle") {
+      this.bottle_collect.play();
     }
   }
 }
