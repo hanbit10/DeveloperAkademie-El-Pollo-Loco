57db370c99a5e3f1108fb6ae8ee7bf4dd0d165b0
hanbit10 <chanbit10@gmail.com>
2024-06-15 20:41:48 +0200

    game menu

--- src/js/game.js
+++ src/js/game.js
@@ -8,6 +8,9 @@ const menuButton = document.getElementById("menu-button");
 const startButton = document.getElementById("start-button");
 const voiceZero = document.getElementById("voice0");
 const voiceOne = document.getElementById("voice1");
+const closeBtn = document.getElementById("close-btn");
+const controlsMenu = document.getElementById("controls-menu");
+const controlsBtn = document.getElementById("controls-button");
 function init() {
   // world = new World(canvas, keyboard);
 
@@ -20,15 +23,23 @@ function init() {
   startButton.addEventListener("click", start);
   voiceOne.addEventListener("click", muted);
   voiceZero.addEventListener("click", unmuted);
+  closeBtn.addEventListener("click", closeControls);
+  controlsBtn.addEventListener("click", showControls);
 
   setInterval(() => {
-    // console.log("gameOverSetting", world.gameOverSetting);
     if (world.gameOverSetting || world.gameWonSetting) {
       menuButton.classList.remove("d-none");
       resetButton.classList.remove("d-none");
       startButton.classList.add("d-none");
     }
-  }, 100);
+  }, 200);
+}
+
+function showControls() {
+  controlsMenu.classList.remove("d-none");
+}
+function closeControls() {
+  controlsMenu.classList.add("d-none");
 }
 
 function muted() {
