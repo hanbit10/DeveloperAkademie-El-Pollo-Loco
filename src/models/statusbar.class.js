2f931614938a5c1387c2bdf6b79d1ba498d53fce
hanbit10 <chanbit10@gmail.com>
2024-06-16 14:54:31 +0200

    update

--- src/models/statusbar.class.js
+++ src/models/statusbar.class.js
@@ -84,22 +84,4 @@ class StatusBar extends DrawableObject {
   resolveImageIndex() {
     return Math.min(5, Math.floor(this.percentage / 20));
   }
-
-  // resolveImageIndex() {
-  //   if (this.percentage >= 100) {
-  //     return 5;
-  //   } else if (this.percentage >= 80) {
-  //     return 4;
-  //   } else if (this.percentage >= 60) {
-  //     return 3;
-  //   } else if (this.percentage >= 40) {
-  //     return 2;
-  //   } else if (this.percentage >= 20) {
-  //     return 1;
-  //   } else if (this.percentage >= 0) {
-  //     return 0;
-  //   } else {
-  //     return 0;
-  //   }
-  // }
 }
