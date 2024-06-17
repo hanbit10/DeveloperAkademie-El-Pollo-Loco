9fe600af6cd63e00de1e6f25b7c36e3e6d4f0b54
hanbit10 <chanbit10@gmail.com>
2024-06-17 14:36:51 +0200

    update

--- src/models/chicken.class.js
+++ src/models/chicken.class.js
@@ -14,6 +14,12 @@ class Chicken extends MoveableObject {
 
   killed_sound = new Audio("../assets/audio/chicken/small-chicken/killed.wav");
   deadSetting = false;
+  /**
+   * Constructs a new instance of the Chicken class with the given id and x position.
+   *
+   * @param {number} id - The unique identifier for the chicken.
+   * @param {number} x - The initial x position of the chicken.
+   */
   constructor(id, x) {
     super().loadImage("../assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
     this.loadImages(this.IMAGES_WALKING);
@@ -22,6 +28,17 @@ class Chicken extends MoveableObject {
     this.id = id;
     this.animate();
   }
+
+  /**
+   * Animate the chicken's movement and actions at regular intervals.
+   *
+   * This function uses `setInterval` to repeatedly execute a callback function at a specified interval.
+   * The callback function checks if the chicken is not dead using the `isDead()` method.
+   * If the chicken is not dead, it calls the `playAnimation()` method with the `IMAGES_WALKING` array as an argument.
+   *
+   *
+   * @return {void} This function does not return a value.
+   */
   animate() {
     setInterval(() => {
       if (!this.isDead()) this.playAnimation(this.IMAGES_WALKING);
@@ -29,6 +46,15 @@ class Chicken extends MoveableObject {
     this.moveAnimation();
   }
 
+  /**
+   * Animate the chicken's movement to the left at regular intervals.
+   *
+   * This function uses `setInterval` to repeatedly execute a callback function at a specified interval.
+   * The callback function checks if the chicken is not dead using the `isDead()` method.
+   * If the chicken is not dead, it calls the `moveLeft()` method.
+   *
+   * @return {void} This function does not return a value.
+   */
   moveAnimation() {
     setInterval(() => {
       if (!this.isDead()) {
@@ -37,6 +63,12 @@ class Chicken extends MoveableObject {
     }, 1000 / 60);
   }
 
+  /**
+   * Plays the killed sound, loads the dead image, sets the speed and energy to 0,
+   * sets the deadSetting flag to true, and moves the chicken to the bottom of the screen after 2 seconds.
+   *
+   * @return {void} No return value
+   */
   dead() {
     this.killed_sound.play();
     this.loadImage("../assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png");
@@ -48,10 +80,26 @@ class Chicken extends MoveableObject {
     }, 2000);
   }
 
+  /**
+   * Pauses the movement of the chicken by setting its speed to 0.
+   *
+   * This function sets the `speed` property of the chicken to 0, effectively pausing its movement.
+   *
+   * @return {void} This function does not return a value.
+   */
   pause() {
     this.speed = 0;
   }
 
+  /**
+   * Resets the state of the object to its initial values.
+   *
+   * This function sets the `deadSetting` property to `false`, generates a new random `x` coordinate within a range of 0 to 100,
+   * sets the `y` coordinate to its initial value, sets the `energy` property to 100, and generates a new random `speed` value
+   * within a range of 0.25 to 0.85.
+   *
+   * @return {void} This function does not return a value.
+   */
   reset() {
     this.deadSetting = false;
     this.x = this.xCache + Math.random() * 100;
@@ -60,10 +108,20 @@ class Chicken extends MoveableObject {
     this.speed = 0.25 + Math.random() * 0.6;
   }
 
+  /**
+   * Mutes the sound of the killed_sound property by setting its volume to 0.
+   *
+   * @return {void} This function does not return a value.
+   */
   mute() {
     this.killed_sound.volume = 0;
   }
 
+  /**
+   * Unmutes the sound of the killed_sound property by setting its volume to 1.
+   *
+   * @return {void} This function does not return a value.
+   */
   unmute() {
     this.killed_sound.volume = 1;
   }
